6e4db3e95b608c137c10c53edc75934990371b93
RAZI01091 <muhammedrazi01091@gmail.com>
2026-06-11 12:35:58 +0530

    Switch to Neon DB

--- settings.py
+++ settings.py
@@ -73,18 +73,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wsgi.application'
 
 
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }  
-
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 
 # PASSWORD VALIDATION
