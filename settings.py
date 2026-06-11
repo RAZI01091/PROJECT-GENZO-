e88c72d9d6d06ff431cfb6d31e33cd1622a6688c
RAZI01091 <muhammedrazi01091@gmail.com>
2026-06-11 11:42:31 +0530

    changed db engine

--- settings.py
+++ settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = 'wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL')
+    )
 }
 
 
