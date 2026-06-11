66bf88031a2e8b2206d08ccd642dff4c2b7ad526
RAZI01091 <muhammedrazi01091@gmail.com>
2026-06-11 14:20:13 +0530

    new changes

--- settings.py
+++ settings.py
@@ -74,10 +74,11 @@ WSGI_APPLICATION = 'wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
-}
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    } 
 
 
 # PASSWORD VALIDATION
