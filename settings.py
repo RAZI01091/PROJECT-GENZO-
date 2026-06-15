7fd010c9695da90adda8071d9e44e2c42e28c99c
RAZI01091 <muhammedrazi01091@gmail.com>
2026-06-15 12:18:57 +0530

    new change for otp

--- settings.py
+++ settings.py
@@ -171,17 +171,18 @@ ACCOUNT_ALLOW_REGISTRATION = True
 
 
 # EMAIL
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_USER = "python010917@gmail.com"
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "python010917@gmail.com"
+
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # MEDIA FILES
